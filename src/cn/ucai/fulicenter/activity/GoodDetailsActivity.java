12df73771c013c569d785a75993708544d8e5b4a
cy885633488 <yuanan.an@foxmail.com>
2016-06-21 18:12:58 +0800

    商品收藏状态初始化

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -15,10 +15,13 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.NetworkImageView;
 
 import cn.ucai.fulicenter.D;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.AlbumBean;
 import cn.ucai.fulicenter.bean.GoodDetailsBean;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.data.ApiParams;
 import cn.ucai.fulicenter.data.GsonRequest;
 import cn.ucai.fulicenter.utils.ImageUtils;
@@ -55,10 +58,10 @@ public class GoodDetailsActivity extends BaseActivity {
     }
 
     private void initData() {
-        int goodId = getIntent().getIntExtra(D.NewGood.KEY_GOODS_ID,0);
+        mGoodsId = getIntent().getIntExtra(D.NewGood.KEY_GOODS_ID,0);
         try {
             String path = new ApiParams()
-                    .with(D.NewGood.KEY_GOODS_ID,goodId+"")
+                    .with(D.NewGood.KEY_GOODS_ID,mGoodsId+"")
                     .getRequestUrl(I.REQUEST_FIND_GOOD_DETAILS);
             executeRequest(new GsonRequest<GoodDetailsBean>(path,GoodDetailsBean.class,
                     responseDownloadGoodDetails(),errorListener()));
@@ -141,5 +144,43 @@ public class GoodDetailsActivity extends BaseActivity {
         WebSettings settings = wvGoodBrief.getSettings();
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         settings.setBuiltInZoomControls(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initCollectStatus();
+    }
+
+    private void initCollectStatus() {
+        User user = FuLiCenterApplication.getInstance().getUser();
+        if (user!=null){
+            // http://10.0.2.2:8080/FuLiCenterServer/Server?request=is_collect&goods_id=&userName=
+            try {
+                String path = new ApiParams()
+                        .with(I.Collect.GOODS_ID,mGoodsId+"")
+                        .with(I.Collect.USER_NAME,FuLiCenterApplication.getInstance().getUserName())
+                        .getRequestUrl(I.REQUEST_IS_COLLECT);
+                executeRequest(new GsonRequest<MessageBean>(path,MessageBean.class,
+                        responseIsCollectListener(),errorListener()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+            mivCollect.setImageResource(R.drawable.bg_collect_in);
+        }
+    }
+
+    private Response.Listener<MessageBean> responseIsCollectListener() {
+        return new Response.Listener<MessageBean>() {
+            @Override
+            public void onResponse(MessageBean messageBean) {
+                if (messageBean.isSuccess()){
+                    mivCollect.setImageResource(R.drawable.bg_collect_out);
+                }else {
+                    mivCollect.setImageResource(R.drawable.bg_collect_in);
+                }
+            }
+        };
     }
 }
